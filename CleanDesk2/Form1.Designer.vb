2f35ae890aa443b3f6de309f2dd2ac092effbe9e
Carlos Fernandez <sirikon2@gmail.com>
2012-09-17 04:16:55 +0200

    All the code

---
+++ CleanDesk2/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(184, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Aceptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(166, 29)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(55, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(166, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Escritorio Limpio"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 25
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.BackgroundImage = Global.CleanDesk2.My.Resources.Resources.Picturesback
+        Me.ClientSize = New System.Drawing.Size(277, 79)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Opacity = 0.0R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Escritorio Limpio"
+        Me.TransparencyKey = System.Drawing.Color.DarkCyan
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ CleanDesk2/Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form1
+    Dim repeats As Integer
+    Dim format As String
+    Dim path As String
+    Dim serial As Integer
+    Dim num2 As Integer
+    Dim data As String
+    Dim pic As String
+    Dim vid As String
+    Dim music As String
+    Dim doc As String
+    Dim pf As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If My.Computer.FileSystem.FileExists("newformats") Then
+            CDConfig.newformats()
+            My.Computer.FileSystem.DeleteFile("newformats")
+        End If
+        If GetSetting("CleanDesk", "data", "configured", "") = "1" Then
+            data = GetSetting("CleanDesk", "data", "time", "")
+            serial = data + 1
+            SaveSetting("CleanDesk", "data", "time", serial)
+            pic = My.Computer.FileSystem.SpecialDirectories.MyPictures
+            vid = My.Computer.FileSystem.GetParentPath(My.Computer.FileSystem.SpecialDirectories.MyDocuments) & "\Videos"
+            doc = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+            pf = My.Computer.FileSystem.SpecialDirectories.ProgramFiles
+            music = My.Computer.FileSystem.SpecialDirectories.MyMusic
+            For number As Integer = 1 To 46
+                If number = 1 Then
+                    format = "jpg"
+                ElseIf number = 2 Then
+                    format = "jpeg"
+                ElseIf number = 3 Then
+                    format = "psd"
+                ElseIf number = 4 Then
+                    format = "png"
+                ElseIf number = 5 Then
+                    format = "bmp"
+                ElseIf number = 6 Then
+                    format = "gif"
+                ElseIf number = 7 Then
+                    format = "tga"
+                ElseIf number = 8 Then
+                    format = "ico"
+                ElseIf number = 9 Then
+                    format = "tif"
+                ElseIf number = 10 Then
+                    format = "tiff"
+                ElseIf number = 11 Then
+                    format = "svg"
+                ElseIf number = 12 Then
+                    format = "cdr"
+                ElseIf number = 13 Then
+                    format = "mp3"
+                ElseIf number = 14 Then
+                    format = "wma"
+                ElseIf number = 15 Then
+                    format = "wmv"
+                ElseIf number = 16 Then
+                    format = "avi"
+                ElseIf number = 17 Then
+                    format = "mp4"
+                ElseIf number = 18 Then
+                    format = "ogv"
+                ElseIf number = 19 Then
+                    format = "webm"
+                ElseIf number = 20 Then
+                    format = "flv"
+                ElseIf number = 21 Then
+                    format = "exe"
+                ElseIf number = 22 Then
+                    format = "msi"
+                ElseIf number = 23 Then
+                    format = "txt"
+                ElseIf number = 24 Then
+                    format = "doc"
+                ElseIf number = 25 Then
+                    format = "odt"
+                ElseIf number = 26 Then
+                    format = "odp"
+                ElseIf number = 27 Then
+                    format = "ods"
+                ElseIf number = 28 Then
+                    format = "pps"
+                ElseIf number = 29 Then
+                    format = "ppsx"
+                ElseIf number = 30 Then
+                    format = "docx"
+                ElseIf number = 31 Then
+                    format = "xls"
+                ElseIf number = 32 Then
+                    format = "xlsx"
+                ElseIf number = 33 Then
+                    format = "dlc"
+                ElseIf number = 34 Then
+                    format = "megamanager"
+                ElseIf number = 35 Then
+                    format = "pdf"
+                ElseIf number = 36 Then
+                    format = "wav"
+                ElseIf number = 37 Then
+                    format = "mkv"
+                ElseIf number = 38 Then
+                    format = "mts"
+                ElseIf number = 39 Then
+                    format = "rtf"
+                ElseIf number = 40 Then
+                    format = "iso"
+                ElseIf number = 41 Then
+                    format = "m4v"
+                ElseIf number = 42 Then
+                    format = "fc2map"
+                ElseIf number = 43 Then
+                    format = "zip"
+                ElseIf number = 44 Then
+                    format = "rar"
+                ElseIf number = 45 Then
+                    format = "7z"
+                ElseIf number = 46 Then
+                    format = "tar.gz"
+                End If
+                path = CDConfig.sel(format)
+                For Each foundFile As String In My.Computer.FileSystem.GetFiles( _
+            My.Computer.FileSystem.SpecialDirectories.Desktop, _
+            FileIO.SearchOption.SearchTopLevelOnly, "*." & format)
+                    Dim foundFileInfo As New System.IO.FileInfo(foundFile)
+                    For Each foundfile2 As String In My.Computer.FileSystem.GetFiles(path, FileIO.SearchOption.SearchTopLevelOnly, "*." & format)
+                        Dim foundFileInfo2 As New System.IO.FileInfo(foundfile2)
+                        If foundFileInfo2.Name = foundFileInfo.Name Then
+                            repeats = repeats + 1
+                            My.Computer.FileSystem.RenameFile(foundfile2, serial & "OLD" + foundFileInfo2.Name)
+                        End If
+                    Next
+                Next
+                For Each foundfile2 As String In My.Computer.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.Desktop, FileIO.SearchOption.SearchTopLevelOnly, "*." & format)
+                    Dim foundFileInfo2 As New System.IO.FileInfo(foundfile2)
+                    My.Computer.FileSystem.MoveFile(foundfile2, path & "\" & foundFileInfo2.Name)
+                Next
+            Next
+            If repeats <> 0 Then
+                Dim habian As String
+                Dim archivos As String
+                habian = "Habían "
+                archivos = " archivos con el nombre repetido, estos an sido renombrados al prefijo OLD"
+                MsgBox(habian & repeats & archivos, MsgBoxStyle.Information, "Repeticiones")
+            End If
+        Else
+            CDConfig.config()
+            MsgBox("CleanDesk configurado, reinicielo para que empiece a funcionar", MsgBoxStyle.Information)
+            Close()
+        End If
+        Timer1.Enabled = True
+        ProgressBar1.Value = 100
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Opacity += 0.1
+        If Me.Opacity = 1 Then Timer1.Enabled = False
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Me.Opacity -= 0.1
+        If Me.Opacity = 0 Then
+            Timer1.Enabled = False
+            Me.Close()
+        End If
+    End Sub
+End Class
